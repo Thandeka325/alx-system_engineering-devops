372ec72e4e9776ce05186b2fdacc92cd5e011a34
Thandeka325 <marhwayiza325@gmail.com>
2025-02-26 03:36:53 -0800

    Task 0: Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,34 +1,5 @@
-# This Puppet manifest fixes Apache 500 error by ensuring PHP is installed and permissions are correct
-
-class apache_fix {
-  package { 'php5':
-    ensure => installed,
-  }
-
-  package { 'libapache2-mod-php5':
-    ensure => installed,
-    require => Package['php5'],
-  }
-
-  file { '/var/www/html/index.php':
-    ensure  => file,
-    content => '<?php phpinfo(); ?>',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0644',
-    require => Package['php5'],
-  }
-
-  exec { 'fix-permissions':
-    command => 'chown -R www-data:www-data /var/www/html && chmod -R 755 /var/www/html',
-    path    => ['/bin', '/usr/bin'],
-  }
-
-  service { 'apache2':
-    ensure  => running,
-    enable  => true,
-    require => Package['php5'],
-  }
+# This Puppet manifest fixes Apache 500 error.
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
-
-include apache_fix
