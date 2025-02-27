c31ec7a7302cc035a4eb34427fb77d1001c3916a
Thandeka325 <marhwayiza325@gmail.com>
2025-02-27 10:05:11 -0800

    Task 0: Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,5 @@
 # Fixes the 500 error
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
-
-exec { 'restart-apache':
-  command => 'systemctl restart apache2',
-  path    => '/sbin/:/bin/:/usr/sbin/:/usr/bin/',
-  require => Exec['fix-wordpress'],
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
